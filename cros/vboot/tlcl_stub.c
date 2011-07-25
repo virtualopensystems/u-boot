0c9c6341aa5b259d18a067e8a9dce8c886ce134e
Che-Liang Chiou <clchiou@chromium.org>
2011-07-25 15:20:30 +0800

    cros: remove CONFIG_HARDWARE_TPM
    Rebased-from: b65adcf
    Lost-file: cros/vboot/tlcl_stub.c
    
    The CONFIG_HARDWARE_TPM flag was used to disable software emulation of
    TPM functionality, for testing purpose or for boards that have a buggy
    or even do not have a hardware TPM. However, it turns out that it is
    easier to mock out TPM entirely in vboot_reference than implement a full
    software-emulated TPM in U-Boot. So we believe remove this flag and
    implement a mock in vboot_reference.
    
    BUG=none
    TEST=boot on Aebl
    
    Change-Id: Id6d2975f04f0f5e4203d45e23f36fce2ad7114f3
    Reviewed-on: http://gerrit.chromium.org/gerrit/4647
    Tested-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- cros/vboot/tlcl_stub.c
+++ cros/vboot/tlcl_stub.c
@@ -18,46 +18,30 @@
 
 VbError_t VbExTpmInit(void)
 {
-/* TODO According to FDT, select the real TPM path or just returning success. */
-#ifdef CONFIG_HARDWARE_TPM
 	if (tis_init())
 		return TPM_E_IOERROR;
-	/* tpm_lite lib doesn't call VbExTpmOpene after VbExTpmInit. */
+	/* tpm_lite lib doesn't call VbExTpmOpen after VbExTpmInit. */
 	return VbExTpmOpen();
-#endif
-	return TPM_SUCCESS;
 }
 
 VbError_t VbExTpmClose(void)
 {
-#ifdef CONFIG_HARDWARE_TPM
 	if (tis_close())
 		return TPM_E_IOERROR;
-#endif
 	return TPM_SUCCESS;
 }
 
 VbError_t VbExTpmOpen(void)
 {
-#ifdef CONFIG_HARDWARE_TPM
 	if (tis_open())
 		return TPM_E_IOERROR;
-#endif
 	return TPM_SUCCESS;
 }
 
 VbError_t VbExTpmSendReceive(const uint8_t* request, uint32_t request_length,
 		uint8_t* response, uint32_t* response_length)
 {
-#ifdef CONFIG_HARDWARE_TPM
 	if (tis_sendrecv(request, request_length, response, response_length))
 		return TPM_E_IOERROR;
-#else
-	/* Make a successful response */
-	uint8_t successful_response[10] =
-			{0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x0, 0x0, 0x0};
-	memcpy(response, successful_response, 10);
-	*response_length = 10;
-#endif
 	return TPM_SUCCESS;
 }
