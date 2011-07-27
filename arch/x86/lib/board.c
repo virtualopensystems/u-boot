118f669a96545f81866cf747b1b3d5a559f17680
Gabe Black <gabeblack@chromium.org>
2011-07-27 16:44:45 -0700

    x86: Initialize the device tree pointer to NULL in i386.
    If the device tree pointer isn't set to anything, which could happen if the
    device tree isn't found, for instance, or simply never set up, the pointer
    could be set to something arbitrary. This change forces initializes it to NULL
    so that it's more obviously not set.
    
    BUG=chrome-os-partner:4993
    TEST=Built and booted u-boot on coreboot, used temporary printfs to print the
    device tree pointer when booting chromeos, and verified that the pointer was
    NULL if not set.
    
    Change-Id: I9c6c34da6f3309b31a1fda920c6b139d728561f3
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/4884
    Reviewed-by: Stefan Reinauer <reinauer@google.com>
    Tested-by: Gabe Black <gabeblack@chromium.org>

--- arch/x86/lib/board.c
+++ arch/x86/lib/board.c
@@ -290,6 +290,8 @@ void board_init_r(gd_t *id, ulong dest_addr)
 	/* compiler optimization barrier needed for GCC >= 3.4 */
 	__asm__ __volatile__("" : : : "memory");
 
+	gd->blob = NULL;
+
 	gd->bd = &bd_data;
 	memset(gd->bd, 0, sizeof(bd_t));
 	bootstage_mark(BOOTSTAGE_ID_BOARD_GLOBAL_DATA);
