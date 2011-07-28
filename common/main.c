2a204f52c5f74bfbc08d7d80ced46df4f5058dc7
Gabe Black <gabeblack@chromium.org>
2011-07-28 04:18:05 -0700

    gen: Tell the FDT library where the device tree is automatically on bringup
    This change adds a call to set_working_fdt_addr near the end of u-boot
    initialization which tells the fdt command/library where the device tree is.
    This makes it possible to use the fdt command to look at the active device tree
    since otherwise there would be no way to know what address it was at to set
    things up manually.
    
    BUG=chrome-os-partner:4993
    TEST=Built for coreboot/Alex, booted, and used fdt to look at the device tree.
    
    Change-Id: Iccd93af0c3934de5f284b76f8c894adc9dbd7f88
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/4919
    Reviewed-by: Gabe Black <gabeblack@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>

--- common/main.c
+++ common/main.c
@@ -44,6 +44,10 @@
 #include <fdt_decode.h>
 #endif /* CONFIG_OF_CONTROL */
 
+#ifdef CONFIG_OF_LIBFDT
+#include <fdt_support.h>
+#endif /* CONFIG_OF_LIBFDT */
+
 #include <post.h>
 #include <linux/ctype.h>
 #include <menu.h>
@@ -412,6 +416,10 @@ void main_loop (void)
 	}
 #endif /* CONFIG_MENUKEY */
 #endif /* CONFIG_BOOTDELAY */
+
+#if defined CONFIG_OF_CONTROL
+	set_working_fdt_addr((void *)gd->blob);
+#endif /* CONFIG_OF_CONTROL */
 
 	/*
 	 * Main Loop for Monitor Command Processing
