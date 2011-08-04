e3c3858b4390a64f81ba99e34b73537a1164076a
Gabe Black <gabeblack@chromium.org>
2011-08-04 13:18:41 -0700

    x86: If the ICH SPI driver is compiled in, initialize it during startup
    BUG=chrome-os-partner:4722
    TEST=Built and booted on x86-alex with the right drivers enabled, saw the SPI
    driver work.
    
    Change-Id: Ie370b622ba2fa9c5a9f1fbc079f45b663cd76b67
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/5378
    
    Conflicts:
    
    	arch/x86/lib/board.c

--- arch/x86/lib/board.c
+++ arch/x86/lib/board.c
@@ -43,6 +43,7 @@
 #include <serial.h>
 #include <asm/u-boot-x86.h>
 #include <asm-generic/sections.h>
+#include <spi.h>
 #include <elf.h>
 
 #ifdef CONFIG_BITBANGMII
@@ -347,6 +348,10 @@ void board_init_r(gd_t *id, ulong dest_addr)
 
 	/* Initialize the console (after the relocation and devices init) */
 	console_init_r();
+
+#ifdef CONFIG_CMD_SPI
+	spi_init();
+#endif
 
 #ifdef CONFIG_MISC_INIT_R
 	show_boot_progress(0x27);
