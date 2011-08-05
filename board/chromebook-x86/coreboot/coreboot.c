8317de5bdf651d961f12fc6585c8ddfd696aab9f
Stefan Reinauer <reinauer@chromium.org>
2011-08-04 17:13:12 -0700

    fixup: Implement missing functions for vboot:
     * video_clear()
       Clear the video framebuffer.
    
     * cold_reboot()
       Do a hard reset through the chipset's reset control register. This
       register is available on all x86 systems (at least those built in
       the last 10ys)
    
     * power_off()
       Power down the machine by using the power management sleep control
       of the chipset. This will currently only work on Intel chipsets.
       However, adapting it to new chipsets is fairly simple. You will
       have to find the IO address of the power management register block
       in your southbridge, and look up the appropriate SLP_TYP_S5 value
       from your southbridge's data sheet.
    
    BUG=chrome-os-partner:3912
    TEST=Run vboot_twostage and see vboot first clear the screen and later cold boot.
         Run poweroff and coldboot command in uboot console.
    
    Change-Id: I59f49a41e8807b2ec3ebb3d083d2a06ced9553a0
    Reviewed-on: http://gerrit.chromium.org/gerrit/5361
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
    Tested-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/18603
    Commit-Ready: Gabe Black (Do Not Use) <gabeblack@google.com>
    Reviewed-by: Gabe Black (Do Not Use) <gabeblack@google.com>
    Tested-by: Gabe Black (Do Not Use) <gabeblack@google.com>

--- board/chromebook-x86/coreboot/coreboot.c
+++ board/chromebook-x86/coreboot/coreboot.c
@@ -36,6 +36,11 @@
 #include <asm/arch-coreboot/sysinfo.h>
 #include <cros/fdt_decode.h>
 #include <cros/firmware_storage.h>
+#include <cros/power_management.h>
+
+#ifdef CONFIG_HW_WATCHDOG
+#include <watchdog.h>
+#endif
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -223,3 +228,19 @@ int board_final_cleanup(void)
 
 	return 0;
 }
+
+int do_coldboot(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+{
+	cold_reboot();
+	return (0);
+}
+
+U_BOOT_CMD(coldboot, 1, 1, do_coldboot, "Initiate a cold reboot.", "");
+
+int do_poweroff(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+{
+	power_off();
+	return (0);
+}
+
+U_BOOT_CMD(poweroff, 1, 1, do_poweroff, "Switch off power", "");
