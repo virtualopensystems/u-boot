d65df81bd87e3527de6fa5493f52c76485926a3f
Simon Glass <sjg@chromium.org>
2012-02-22 10:37:21 -0800

    cros: tegra: Add Chrome OS config file for Tegra Seaboard
    Add a file to control the config of a Seaboard (or variant) running
    Chrome OS.
    
    Change-Id: I94db425535397f76abe2d714f69a86e1ec4f01fe

---
+++ include/configs/chromeos_seaboard.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __configs_chromeos_seaboard_h__
+#define __configs_chromeos_seaboard_h__
+
+/* So far all our tegra2-based boards share the seaboard config. */
+#include <configs/seaboard.h>
+
+/* We don't need to fully init the LCD - verified boot does this */
+#undef BOARD_LATE_INIT
+
+/* Support USB booting */
+#define CONFIG_CHROMEOS_USB
+
+#include "chromeos.h"
+
+/* We initialize Chrome OS -specific GPIOs here */
+#define CONFIG_MISC_INIT_R
+
+/* Store the VbNvContext in the first block of the disk. */
+#define CHROMEOS_VBNVCONTEXT_LBA	0
+
+/* Delay console init until after relocation (saves boot time) */
+#define CONFIG_DELAY_CONSOLE
+
+#endif /* __configs_chromeos_seaboard_h__ */
