339c422b56e109d1a213cf7f305ab2a5d849db86
Gabe Black <gabeblack@chromium.org>
2012-02-24 01:56:30 -0800

    fixup: lost: split: gen: x86: Clean up MTRR 7 right before jumping to the kernel
    BUG=chromium-os:26787
    TEST=Built
    
    Signed-off-by: Gabe Black <gabeblack@google.com>
    
    Change-Id: I33632da97a33051f5808e1d4ba7ec4a72175ede9
    Reviewed-on: https://gerrit.chromium.org/gerrit/16706
    Reviewed-by: Gabe Black (Do Not Use) <gabeblack@google.com>
    Tested-by: Gabe Black (Do Not Use) <gabeblack@google.com>

--- arch/x86/include/asm/cache.h
+++ arch/x86/include/asm/cache.h
@@ -36,14 +36,15 @@
 /* The memory clobber prevents the GCC from reordering the read/write order
  * of CR0
  */
-static inline unsigned long read_cr0(void)
+static inline unsigned long x86_cache_read_cr0(void)
 {
 	unsigned long cr0;
+
 	asm volatile ("movl %%cr0, %0" : "=r" (cr0) : : "memory");
 	return cr0;
 }
 
-static inline void write_cr0(unsigned long cr0)
+static inline void x86_cache_write_cr0(unsigned long cr0)
 {
 	asm volatile ("movl %0, %%cr0" : : "r" (cr0) : "memory");
 }
@@ -61,19 +62,21 @@ static inline void invd(void)
 static inline void enable_cache(void)
 {
 	unsigned long cr0;
-	cr0 = read_cr0();
+
+	cr0 = x86_cache_read_cr0();
 	cr0 &= 0x9fffffff;
-	write_cr0(cr0);
+	x86_cache_write_cr0(cr0);
 }
 
 static inline void disable_cache(void)
 {
 	/* Disable and write back the cache */
 	unsigned long cr0;
-	cr0 = read_cr0();
+
+	cr0 = x86_cache_read_cr0();
 	cr0 |= 0x40000000;
 	wbinvd();
-	write_cr0(cr0);
+	x86_cache_write_cr0(cr0);
 	wbinvd();
 }
 
