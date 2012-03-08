099540f15da49ca3b96dd678818740bf3c6f2e10
Abhilash Kesavan <a.kesavan@samsung.com>
2012-03-07 16:09:40 -0800

    spi: Add W25Q32 to Winbond SPI flash table
    Daisy has a different Winbond part as compared to the SMDK.
    Add the part details to the SPI flash table.
    
    CQ-DEPEND=I23328e2117937b90f6912738fb54567f8a64c14a
    Change-Id: I0158419632ee1de9b64e6aa25256f87494818f46
    Signed-off-by: Abhilash Kesavan <a.kesavan@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/17558
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Che-Liang Chiou <clchiou@chromium.org>

--- drivers/mtd/spi/winbond.c
+++ drivers/mtd/spi/winbond.c
@@ -107,6 +107,14 @@ static const struct winbond_spi_flash_params winbond_spi_flash_table[] = {
 		.nr_blocks		= 128,
 		.name			= "W25Q80",
 	},
+	{
+		.id			= 0x6016,
+		.l2_page_size		= 8,
+		.pages_per_sector	= 16,
+		.sectors_per_block	= 16,
+		.nr_blocks		= 64,
+		.name			= "W25Q32",
+	},
 
 };
 
