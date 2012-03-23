36f4b0f3a109720b33c162cd7a98cd1670aec536
Hatim Ali <hatim.rv@samsung.com>
2012-03-23 12:06:58 -0700

    exynos: Correcting wrong GPIO register offset.
    The patch corrects the GPIO offset for GPX series of GPIO registers.
    
    Change-Id: I98d2fd5da48b05f63670bec5068c03d2267a9f07
    Signed-off-by: Hatim Ali <hatim.rv@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/19021
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Ready: Banajit Goswami <banajit.g@samsung.com>

--- arch/arm/include/asm/arch-exynos5/gpio.h
+++ arch/arm/include/asm/arch-exynos5/gpio.h
@@ -45,7 +45,7 @@ struct exynos5_gpio_part1 {
 	struct s5p_gpio_bank y4;
 	struct s5p_gpio_bank y5;
 	struct s5p_gpio_bank y6;
-	struct s5p_gpio_bank res1[0x980];
+	char res1[0x980];
 	struct s5p_gpio_bank x0;
 	struct s5p_gpio_bank x1;
 	struct s5p_gpio_bank x2;
