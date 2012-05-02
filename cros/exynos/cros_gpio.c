a96c9cb8d4aa61549437c59beb60730a19b0df01
Che-Liang Chiou <clchiou@chromium.org>
2012-05-02 19:28:44 +0800

    cros: exynos: Add board-specific GPIO setup
    The exynos GPIO needs pull mode set (to none or whatever) before read.
    
    Signed-off-by: Che-Liang Chiou <clchiou@chromium.org>
    
    BUG=chromium-os:28077
    TEST=run `vbexport_test init && vboot_test gpio` and check GPIO readings
         on daisy on alex
    
    Sample output of vboot_test gpio
    >>>>
    cros_gpio_debug.c: cros_gpio_dump: wpsw  : port=102, polarity=0, value=0
    cros_gpio_debug.c: cros_gpio_dump: recsw : port=112, polarity=0, value=0
    cros_gpio_debug.c: cros_gpio_dump: devsw : port=168, polarity=1, value=0
    <<<<
    
    Change-Id: I925bc2e66e6379f680dd5f0dbcbaec45020f88ba
    Reviewed-on: https://gerrit.chromium.org/gerrit/21615
    Tested-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Ready: Che-Liang Chiou <clchiou@chromium.org>

---
+++ cros/exynos/cros_gpio.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+/* Implementation of per-board GPIO accessor functions */
+
+#include <common.h>
+#include <asm/arch/gpio.h>
+#include <cros/common.h>
+#include <cros/cros_gpio.h>
+
+int cros_gpio_setup(enum cros_gpio_index index, int port)
+{
+	gpio_set_pull(port, GPIO_PULL_NONE);
+	return 0;
+}
