a4d1d38bfb708455ccd6e45c6d3dd15a809b90e4
Tom Wai-Hong Tam <waihong@chromium.org>
2012-05-04 11:45:01 +0800

    exynos: spl: Add the default value of serial base to machine parameter
    We assign the serial base address as the port 3 serial address.
    
    BUG=chrome-os-partner:9021
    TEST=build and run u-boot successfully; detailed test pending to later CLs.
    
    Change-Id: Ibb9cdd9485acc06891f2c0073f71ab258c800b8c
    Signed-off-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/21850

--- board/samsung/smdk5250/smdk5250_spl.c
+++ board/samsung/smdk5250/smdk5250_spl.c
@@ -32,7 +32,7 @@ static struct spl_machine_param machine_param
 		__attribute__((section(".machine_param"))) = {
 	.signature	= SIGNATURE,
 	.version	= 1,
-	.params		= "vmubfa",
+	.params		= "vmubfas",
 	.size		= sizeof(machine_param),
 
 	.mem_type	= DDR_MODE_DDR3,
@@ -51,6 +51,7 @@ static struct spl_machine_param machine_param
 	.boot_source	= BOOT_MODE_OM,
 	.frequency_mhz	= 800,
 	.arm_freq_mhz	= 1700,
+	.serial_base	= 0x12c30000,
 };
 
 struct spl_machine_param *spl_get_machine_params(void)
