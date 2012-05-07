545b26177ed863282ec254a407a4e12147a52d8b
Vic Yang <victoryang@chromium.org>
2012-05-07 16:53:57 +0800

    Add a u-boot command to fill in ACPI value when network boot
    When network booting, we don't have verified boot. Add this command to
    fill in ACPI mainfw_type value so that we can identify we are network
    booting.
    
    BUG=chrome-os-partner:7952
    TEST=Manual
    
    Change-Id: I5da88c82e5a6a20b21ebf0b18273653d5ccef6d6
    Reviewed-on: https://gerrit.chromium.org/gerrit/21965
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>
    Commit-Ready: Vic Yang <victoryang@chromium.org>
    Tested-by: Vic Yang <victoryang@chromium.org>

---
+++ cros/cmd/cmd_netboot.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+#include <common.h>
+#include <command.h>
+#include <fdtdec.h>
+#include <fdt_support.h>
+#include <libfdt.h>
+#include <asm/arch-coreboot/sysinfo.h>
+#include <cros/common.h>
+#include <cros/cros_fdtdec.h>
+#include <cros/crossystem_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static int
+do_netboot_acpi(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	/* TODO: fdt should be retrieved from gd->fdt_blob. But somehow this
+	 * seems to point to CBFS and lose info from coreboot. This workaround
+	 * read fdt from sysinfo. */
+	const void *fdt = lib_sysinfo.sys_fdt;
+	int node_offset;
+	const uint32_t *cell;
+	chromeos_acpi_t *acpi_table;
+
+	node_offset = cros_fdtdec_config_node(fdt);
+	if (node_offset < 0)
+		return 1;
+
+	cell = fdt_getprop(fdt, node_offset, "gnvs-vboot-table", NULL);
+	if (!cell) {
+		VBDEBUG("Couldn't access gnvs-vboot-table.\n");
+		return 1;
+	}
+
+	acpi_table = (chromeos_acpi_t *)(uintptr_t)ntohl(*cell);
+
+	acpi_table->vbt7 = 3;
+
+	return 0;
+}
+
+U_BOOT_CMD(netboot_acpi, 1, 1, do_netboot_acpi,
+		"Fill in Network boot ACPI value", NULL);
