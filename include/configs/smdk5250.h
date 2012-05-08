0ef0f8da65b4e8677966fa5b49470b1bb26848cd
Doug Anderson <dianders@chromium.org>
2012-05-08 15:59:39 -0700

    config: exynos: enable g3d voltage to 1.2v
    Set G3D voltage to 1.2 v
    
    BUG=none
    TEST=build ok, booted on smdk
    
    Change-Id: Iead5af5c6e9a52de799c27dae2cfce2585c81d86
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    (cherry picked from commit e8eda196b1c1cb73a6163729ec1da9e3ae23ddbd)
    Reviewed-on: https://gerrit.chromium.org/gerrit/22238
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/configs/smdk5250.h
+++ include/configs/smdk5250.h
@@ -133,6 +133,7 @@
 #define CONFIG_VDD_INT		1150 /* 1.15v */
 #define CONFIG_VDD_MIF		1200 /* 1.2v */
 #define CONFIG_VDD_LDO21	1350 /* 1.35v */
+#define CONFIG_VDD_G3D		1200 /* 1.2 v */
 #define CONFIG_VDD_LDO2		1500 /* 1.5v */
 #define CONFIG_VDD_LDO3		1800 /* 1.8v */
 #define CONFIG_VDD_LDO5		1800 /* 1.8v */
