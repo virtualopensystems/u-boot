6207cdedfdc5f735e8520c1b0df43d35acbcf636
Simon Glass <sjg@chromium.org>
2012-05-14 13:26:51 -0700

    exynos: Add board revision GPIOs for smdk5250
    Add the default settings for this setting for smdk5250.
    
    BUG=chrome-os-partner:9653
    TEST=manual:
    build and boot on daisy, see that it is 0
    Need to test on a modded board to see other values
    
    Change-Id: I33ce4a8cbe6f3ed115d02cfe43975b241021e916
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/22623
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- board/samsung/smdk5250/smdk5250_spl.c
+++ board/samsung/smdk5250/smdk5250_spl.c
@@ -24,6 +24,7 @@
 #include <asm/arch-exynos/cpu.h>
 #include <asm/arch-exynos/spl.h>
 #include <asm/arch/dmc.h>
+#include <asm/arch/gpio.h>
 
 #define SIGNATURE	0xdeadbeef
 
@@ -32,7 +33,7 @@ static struct spl_machine_param machine_param
 		__attribute__((section(".machine_param"))) = {
 	.signature	= SIGNATURE,
 	.version	= 1,
-	.params		= "vmubfasi",
+	.params		= "vmubfasir",
 	.size		= sizeof(machine_param),
 
 	.mem_type	= DDR_MODE_DDR3,
@@ -53,6 +54,7 @@ static struct spl_machine_param machine_param
 	.arm_freq_mhz	= 1700,
 	.serial_base	= 0x12c30000,
 	.i2c_base	= 0x12c60000,
+	.board_rev_gpios = GPIO_D00 | (GPIO_D01 << 16),
 };
 
 struct spl_machine_param *spl_get_machine_params(void)
