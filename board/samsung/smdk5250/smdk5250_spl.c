8ce3f600c87f991fff9f1f3d51a30a0ae1ad52cc
Alim Akhtar <alim.akhtar@samsung.com>
2012-05-17 21:03:43 -0700

    exynos: add memory manufacturer in machine parameters
    Add samsung as memory manufacturer in machine parameters
    
    BUG=chrome-os-partner:9765
    TEST=build ok, booted on snow
    
    Change-Id: Ib97b2b0c5c6079f43d4f86684ced03cdd92e7dfc
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/23019
    Commit-Ready: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>

--- board/samsung/smdk5250/smdk5250_spl.c
+++ board/samsung/smdk5250/smdk5250_spl.c
@@ -34,7 +34,7 @@ static struct spl_machine_param machine_param
 		__attribute__((section(".machine_param"))) = {
 	.signature	= SIGNATURE,
 	.version	= 1,
-	.params		= "vmubfasir",
+	.params		= "vmubfasirM",
 	.size		= sizeof(machine_param),
 
 	.mem_type	= DDR_MODE_DDR3,
@@ -56,6 +56,7 @@ static struct spl_machine_param machine_param
 	.serial_base	= 0x12c30000,
 	.i2c_base	= 0x12c60000,
 	.board_rev_gpios = GPIO_D00 | (GPIO_D01 << 16),
+	.mem_manuf	= MEM_MANUF_SAMSUNG,
 };
 
 struct spl_machine_param *spl_get_machine_params(void)
