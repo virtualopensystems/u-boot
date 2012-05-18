610b4ab127ea4faed697ece79ff42e34297a05c2
Alim Akhtar <alim.akhtar@samsung.com>
2012-05-17 20:43:53 -0700

    exynos: add machine parameter for manufacturer name
    Add a new machine parameter which specifies the memory manufacturer,
    now snow has tmo kind of memory one from elpida and another from
    samsung
    
    BUG=chrome-os-partner:9765
    TEST=builok ok, booted on snow
    
    Change-Id: Ia3055eac91e0c8ce8cbb85dcea0f300361e8a33d
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/23018
    Commit-Ready: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>

--- arch/arm/include/asm/arch-exynos/spl.h
+++ arch/arm/include/asm/arch-exynos/spl.h
@@ -50,6 +50,7 @@ struct spl_machine_param {
 	 * i		i2c base address for early access (meant for PMIC)
 	 * r		board rev GPIO numbers used to read board revision
 	 *			(lower halfword=bit 0, upper=bit 1)
+	 * M		Memory Manufacturer name
 	 * \0		termination
 	 */
 	char		params[12];	/* Length must be word-aligned */
@@ -69,6 +70,7 @@ struct spl_machine_param {
 	u32		serial_base;	/* Serial base address */
 	u32		i2c_base;	/* i2c base address */
 	u32		board_rev_gpios;	/* Board revision GPIOs */
+	enum mem_manuf	mem_manuf;	/* Memory Manufacturer */
 } __attribute__((__packed__));
 
 /**
