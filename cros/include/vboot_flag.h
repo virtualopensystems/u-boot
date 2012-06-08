a782be4e11e948b6900d0d7dc42b67f94ed08280
Tom Wai-Hong Tam <waihong@chromium.org>
2012-06-08 15:34:54 +0800

    cros: [1/4] vboot_flag: Implement basic prototype of vboot_flag library
    This is the basic prototype of the vboot_flag library. We use the
    vboot_flag_driver struct to encrypt the detailed driver implementation,
    like setup() and fetch().
    
    At vboot_flag_init(), it decodes the device tree. For each vboot_flag node,
    according the compat field, it selects the correct driver.
    
    We break the whole large change into 4 CLs for easily review.
    Be care that only submitting one of them will break the build.
    
    This is one of the series of patches that refactors GPIO interface to more
    generic VBOOT flag interface, which accepts more input source besides
    hardware GPIO, like sysinfo from coreboot, constant value, and value from ec
    
    BUG=chrome-os-partner:9706,9183,10175
    TEST=Built successfully on Daisy and Link. Run GPIO debug command in console:
    SMDK5250 # vbexport_test init
    SMDK5250 # vboot_test gpio
    vboot_flag_debug.c: vboot_flag_dump: vboot-flag-write-protect: port=102, active_high=0, value=1
    vboot_flag_debug.c: vboot_flag_dump: vboot-flag-recovery     : port=112, active_high=0, value=0
    vboot_flag_debug.c: vboot_flag_dump: vboot-flag-developer    : port=168, active_high=1, value=0
    vboot_flag_debug.c: vboot_flag_dump: vboot-flag-lid-open     : port=  0, active_high=0, value=1
    vboot_flag_debug.c: vboot_flag_dump: vboot-flag-power-off    : port=  0, active_high=0, value=0
    
    Change-Id: Iee3968f5a3aadc4d10c503ae19645f2d34950941
    Signed-off-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/24863

--- cros/include/vboot_flag.h
+++ cros/include/vboot_flag.h
@@ -13,6 +13,8 @@
 #ifndef __VBOOT_FLAG_H__
 #define __VBOOT_FLAG_H__
 
+#include <fdtdec.h>
+
 enum vboot_flag_id {
 	VBOOT_FLAG_WRITE_PROTECT = 0,
 	VBOOT_FLAG_RECOVERY,
@@ -22,6 +24,70 @@ enum vboot_flag_id {
 
 	VBOOT_FLAG_MAX_FLAGS
 };
+
+/* The struct to store the context for each flag */
+struct vboot_flag_context {
+	struct vboot_flag_driver *driver;	/* the driver for ths eflag */
+	int config_node;	/* offset of the config node in fdt */
+	int node;		/* offset of the vboot flag node in fdt */
+};
+
+/* GPIO details required in the crossystem data structure */
+struct vboot_flag_details {
+	int value;		/* the value of the flag, either 1 or 0 */
+	int port;		/* the gpio port number */
+	int active_high;	/* 1 if the port is active high; 0 active low */
+};
+
+/* API function pointers of the driver to get vboot flag info */
+struct vboot_flag_driver {
+	/* the fdt compatible type */
+	enum fdt_compat_id type;
+	/**
+	 * The setup() function will be call in vboot_flag_init()
+	 *
+	 * @prarm id		ID of VBoot flag
+	 * @param context	Pointer to the context which keeps internal data
+	 * @return zero on success and non-zero on failures
+	 */
+	int (*setup)(enum vboot_flag_id id, struct vboot_flag_context *context);
+	/**
+	 * The fetch() function returns the details of the vboot flag
+	 *
+	 * @prarm id		ID of VBoot flag
+	 * @param context	Pointer to the context which keeps internal data
+	 @ @param details	Returns the details of the flag
+	 * @return zero on success and non-zero on failures
+	 */
+	int (*fetch)(enum vboot_flag_id id, struct vboot_flag_context *context,
+			struct vboot_flag_details *details);
+};
+
+/**
+ * Get the node name of the vboot flag
+ *
+ * @prarm id		ID of VBoot flag
+ * @return the node name string for the flag
+ */
+const char *vboot_flag_node_name(enum vboot_flag_id id);
+
+/**
+ * Initialize vboot_flag module
+ *
+ * @return zero on success and non-zero on failures
+ */
+int vboot_flag_init(void);
+
+/**
+ * Fetch the details of the required vboot flag
+ *
+ * @param id		ID of VBoot flag
+ * @param details	Returns the GPIO details
+ * @return zero on success and non-zero on failures
+ */
+int vboot_flag_fetch(enum vboot_flag_id id, struct vboot_flag_details *details);
+
+/* TODO(waihong) Remove the legacy below when vboot flag refactoring done */
 
 enum cros_gpio_polarity {
 	CROS_GPIO_ACTIVE_LOW	= 0,
---
+++ cros/lib/vboot_flag.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <asm/global_data.h>
+#include <cros/common.h>
+#include <cros/cros_fdtdec.h>
+#include <cros/vboot_flag.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static struct vboot_flag_context internal_context[VBOOT_FLAG_MAX_FLAGS];
+
+/* The drivers supported are listed here */
+static struct vboot_flag_driver vboot_flag_driver_unknown = {
+	.type	= COMPAT_UNKNOWN,
+	.setup	= NULL,
+	.fetch	= NULL,
+};
+
+static const char *node_name[VBOOT_FLAG_MAX_FLAGS] = {
+	"vboot-flag-write-protect",
+	"vboot-flag-recovery",
+	"vboot-flag-developer",
+	"vboot-flag-lid-open",
+	"vboot-flag-power-off",
+};
+
+const char *vboot_flag_node_name(enum vboot_flag_id id)
+{
+	return node_name[id];
+}
+
+static struct vboot_flag_context *vboot_flag_get_context(enum vboot_flag_id id)
+{
+	if (id < 0 || id >= VBOOT_FLAG_MAX_FLAGS) {
+		VBDEBUG("id out of range: %d\n", id);
+		return NULL;
+	}
+	return &internal_context[id];
+}
+
+static int vboot_flag_setup(enum vboot_flag_id id)
+{
+	struct vboot_flag_context *context = vboot_flag_get_context(id);
+
+	if (context == NULL)
+		return -1;
+
+	if (context->driver) {
+		if (context->driver->setup)
+			return context->driver->setup(id, context);
+		else
+			return 0; /* setup() is unnecessary */
+	}
+
+	VBDEBUG("the driver of %s not assigned\n", node_name[id]);
+	return -1;
+}
+
+int vboot_flag_fetch(enum vboot_flag_id id, struct vboot_flag_details *details)
+{
+	struct vboot_flag_context *context = vboot_flag_get_context(id);
+
+	if (context == NULL)
+		return -1;
+
+	if (context->driver && context->driver->fetch)
+		return context->driver->fetch(id, context, details);
+
+	VBDEBUG("the driver of %s not assigned\n", node_name[id]);
+	return -1;
+}
+
+int vboot_flag_init(void)
+{
+	const void *blob = gd->fdt_blob;
+	int config, child;
+	int i;
+	struct vboot_flag_context *context;
+
+	config = cros_fdtdec_config_node(blob);
+	if (config < 0)
+		return -1;
+
+	for (i = 0; i < VBOOT_FLAG_MAX_FLAGS; i++) {
+		context = &internal_context[i];
+
+		/* By default, assign an unknown driver which does nothing */
+		child = fdt_subnode_offset(blob, config, node_name[i]);
+		if (child < 0) {
+			VBDEBUG("can't find the node %s\n", node_name[i]);
+			continue;
+		}
+		context->node = child;
+		context->config_node = config;
+		context->driver = &vboot_flag_driver_unknown;
+
+		/* TODO(waihong) Assign the correct drivers when implemented */
+		switch (fdtdec_lookup(blob, child)) {
+		default:
+			VBDEBUG("can't find any compatiable driver %s\n",
+				node_name[i]);
+		}
+	}
+
+	/* Call the setup function for each driver */
+	for (i = 0; i < VBOOT_FLAG_MAX_FLAGS; i++)
+		if (vboot_flag_setup(i))
+			VBDEBUG("driver setup failed %s\n", node_name[i]);
+
+	return 0;
+}
