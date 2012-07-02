31ea833120bae497fa94d0706b94ea27eae18042
Liam McLoughlin <google.com>
2012-07-02 14:12:47 -0700

    drop: cros: added amxx files
    BUG=none
    TEST=Build chromeos_beaglebone board (requires other two commits)
    
    Signed-off-by: Liam McLoughlin <lmcloughlin@google.com>
    Change-Id: Ib799a4a83e03312437defe4fdbeb084a959c2193
    Reviewed-on: https://gerrit.chromium.org/gerrit/26605
    Tested-by: Liam McLoughlin <lmcloughlin@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Ready: Liam McLoughlin <lmcloughlin@chromium.org>

---
+++ cros/amxx/utility.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+#include <common.h>
+#include <cros/common.h>
+
+#include <vboot_api.h>
+
+uint64_t VbExGetTimer(void)
+{
+	return timer_get_us();
+}
