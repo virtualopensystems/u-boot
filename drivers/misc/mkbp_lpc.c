a35977bf0165d5be4aa5225ebf03e5d10cbf2f4e
Randall Spangler <rspangler@chromium.org>
2012-07-19 14:50:14 -0700

    Support command args on LPC protocol
    BUG=chrome-os-partner:11319
    TEST=build link; mkbp hash from u-boot console
    
    Change-Id: Ia407785b1a6e4352aaac4da4b75f1690d6f65d09
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/27932
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>

--- drivers/misc/mkbp_lpc.c
+++ drivers/misc/mkbp_lpc.c
@@ -68,7 +68,7 @@ static int wait_for_sync(struct mkbp_dev *dev)
  * @param din_len       Maximum size of response in bytes
  * @return number of bytes in response, or -1 on error
  */
-int mkbp_lpc_command(struct mkbp_dev *dev, uint8_t cmd, int cmd_version,
+static int old_lpc_command(struct mkbp_dev *dev, uint8_t cmd,
 		     const uint8_t *dout, int dout_len,
 		     uint8_t *din, int din_len)
 {
@@ -119,6 +119,128 @@ int mkbp_lpc_command(struct mkbp_dev *dev, uint8_t cmd, int cmd_version,
 }
 
 /**
+ * Send a command to a LPC MKBP device and return the reply.
+ *
+ * The device's internal input/output buffers are used.
+ *
+ * @param dev		MKBP device
+ * @param cmd		Command to send (EC_CMD_...)
+ * @param cmd_version	Version of command to send (EC_VER_...)
+ * @param dout          Output data (may be NULL If dout_len=0)
+ * @param dout_len      Size of output data in bytes
+ * @param din           Response data (may be NULL If din_len=0)
+ * @param din_len       Maximum size of response in bytes
+ * @return number of bytes in response, or -1 on error
+ */
+int mkbp_lpc_command(struct mkbp_dev *dev, uint8_t cmd, int cmd_version,
+		     const uint8_t *dout, int dout_len,
+		     uint8_t *din, int din_len)
+{
+	const int cmd_addr = EC_LPC_ADDR_HOST_CMD;
+	const int data_addr = EC_LPC_ADDR_HOST_DATA;
+	const int args_addr = EC_LPC_ADDR_HOST_ARGS;
+	const int param_addr = EC_LPC_ADDR_HOST_PARAM;
+
+	struct ec_lpc_host_args args;
+	uint8_t *d;
+	int csum;
+	int i;
+
+	/* Fall back to old-style command interface if args aren't supported */
+	if (!dev->cmd_version_is_supported)
+		return old_lpc_command(dev, cmd, dout, dout_len, din, din_len);
+
+	if (dout_len > EC_HOST_PARAM_SIZE) {
+		debug("%s: Cannot send %d bytes\n", __func__, dout_len);
+		return -1;
+	}
+
+	/* Fill in args */
+	args.flags = EC_HOST_ARGS_FLAG_FROM_HOST;
+	args.command_version = cmd_version;
+	args.data_size = dout_len;
+
+	/* Calculate checksum */
+	csum = cmd + args.flags + args.command_version + args.data_size;
+	for (i = 0, d = (uint8_t *)dout; i < dout_len; i++, d++)
+		csum += *d;
+
+	args.checksum = (uint8_t)csum;
+
+	if (wait_for_sync(dev)) {
+		debug("%s: Timeout waiting ready\n", __func__);
+		return -1;
+	}
+
+	/* Write args */
+	for (i = 0, d = (uint8_t *)&args; i < sizeof(args); i++, d++)
+		outb(*d, args_addr + i);
+
+	/* Write data, if any */
+	debug_trace("cmd: %02x, ver: %02x", cmd, cmd_version);
+	for (i = 0, d = (uint8_t *)dout; i < dout_len; i++, d++) {
+		outb(*d, param_addr + i);
+		debug_trace("%02x ", *d);
+	}
+
+	outb(cmd, cmd_addr);
+	debug_trace("\n");
+
+	if (wait_for_sync(dev)) {
+		debug("%s: Timeout waiting for response\n", __func__);
+		return -1;
+	}
+
+	/* Check result */
+	i = inb(data_addr);
+	if (i) {
+		debug("%s: MKBP result code %d\n", __func__, i);
+		return -i;
+	}
+
+	/* Read back args */
+	for (i = 0, d = (uint8_t *)&args; i < sizeof(args); i++, d++)
+		*d = inb(args_addr + i);
+
+	/*
+	 * If EC didn't modify args flags, then somehow we sent a new-style
+	 * command to an old EC, which means it would have read its params
+	 * from the wrong place.
+	 */
+	if (!(args.flags & EC_HOST_ARGS_FLAG_TO_HOST)) {
+		debug("%s: MKBP protocol mismatch\n", __func__);
+		return -EC_RES_INVALID_RESPONSE;
+	}
+
+	if (args.data_size > din_len) {
+		debug("%s: MKBP returned too much data %d > %d\n",
+		      __func__, args.data_size, din_len);
+		return -EC_RES_INVALID_RESPONSE;
+	}
+
+	/* Read data, if any */
+	debug_trace("resp: %02x, ", ret);
+	for (i = 0, d = (uint8_t *)din; i < args.data_size; i++, d++) {
+		*d = inb(param_addr + i);
+		debug_trace("%02x ", *d);
+	}
+	debug_trace("\n");
+
+	/* Verify checksum */
+	csum = cmd + args.flags + args.command_version + args.data_size;
+	for (i = 0, d = (uint8_t *)din; i < args.data_size; i++, d++)
+		csum += *d;
+
+	if (args.checksum != (uint8_t)csum) {
+		debug("%s: MKBP response has invalid checksum\n", __func__);
+		return -EC_RES_INVALID_CHECKSUM;
+	}
+
+	/* Return actual amount of data received */
+	return args.data_size;
+}
+
+/**
  * Initialize LPC protocol.
  *
  * @param dev		MKBP device
@@ -140,15 +262,7 @@ int mkbp_lpc_init(struct mkbp_dev *dev, const void *blob)
 	    (inb(EC_LPC_ADDR_MEMMAP + EC_MEMMAP_HOST_CMD_FLAGS) &
 	     EC_HOST_CMD_FLAG_LPC_ARGS_SUPPORTED)) {
 		/* A MKBP-capable EC is present and supports command args */
-#ifdef CONFIG_MKBP_LPC_ARGS_SUPPORTED
 		dev->cmd_version_is_supported = 1;
-#else
-		/*
-		 * However, WE don't support command args yet, so ignore that
-		 * capability for now.  crosbug.com/p/11319
-		 */
-		dev->cmd_version_is_supported = 0;
-#endif
 	} else {
 		/* It's not an EC which supports command args. */
 		int byte = 0xff;
