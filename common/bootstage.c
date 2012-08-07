1d78c455dd28b0d8e4e813872bd81583cf93ae0e
Bill Richardson <wfrichar@chromium.org>
2012-08-06 17:02:14 -0700

    Revert "gen: bootstage: Export bootstage_add_record() function"
    Reverts "gen: bootstage: Export bootstage_add_record() function"
    
    This function is not static, but not exported either. Add a prototype
    in the header file and move the required enum to the header also.
    
    BUG=chromium-os:31642
    TEST=build and boot on snow
    
    Reverted change id: I68c668bbeff88dbe7b39246123ab3ea8537bc738
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/28805
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>
    Change-Id: Ib443b99bac8d7b5acf4f3fdb1daad528c59bb5bf
    Reviewed-on: https://gerrit.chromium.org/gerrit/29359
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>
    Tested-by: Bill Richardson <wfrichar@chromium.org>

--- common/bootstage.c
+++ common/bootstage.c
@@ -33,9 +33,13 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+enum bootstage_flags {
+	BOOTSTAGEF_ERROR	= 1 << 0,	/* Error record */
+	BOOTSTAGEF_ALLOC	= 1 << 1,	/* Allocate an id */
+};
+
 struct bootstage_record {
 	ulong time_us;
-	uint32_t start_us;
 	const char *name;
 	int flags;		/* see enum bootstage_flags */
 	enum bootstage_id id;
@@ -45,9 +49,10 @@ static struct bootstage_record record[BOOTSTAGE_ID_COUNT] = { {1} };
 static int next_id = BOOTSTAGE_ID_USER;
 
 ulong bootstage_add_record(enum bootstage_id id, const char *name,
-			   int flags, ulong mark)
+			   int flags)
 {
 	struct bootstage_record *rec;
+	ulong mark = timer_get_boot_us();
 
 	if (flags & BOOTSTAGEF_ALLOC)
 		id = next_id++;
@@ -72,13 +77,12 @@ ulong bootstage_add_record(enum bootstage_id id, const char *name,
 
 ulong bootstage_mark(enum bootstage_id id)
 {
-	return bootstage_add_record(id, NULL, 0, timer_get_boot_us());
+	return bootstage_add_record(id, NULL, 0);
 }
 
 ulong bootstage_error(enum bootstage_id id)
 {
-	return bootstage_add_record(id, NULL, BOOTSTAGEF_ERROR,
-				    timer_get_boot_us());
+	return bootstage_add_record(id, NULL, BOOTSTAGEF_ERROR);
 }
 
 ulong bootstage_mark_name(enum bootstage_id id, const char *name)
@@ -87,7 +91,7 @@ ulong bootstage_mark_name(enum bootstage_id id, const char *name)
 
 	if (id == BOOTSTAGE_ID_ALLOC)
 		flags = BOOTSTAGEF_ALLOC;
-	return bootstage_add_record(id, name, flags, timer_get_boot_us());
+	return bootstage_add_record(id, name, flags);
 }
 
 static void print_time(unsigned long us_time)
