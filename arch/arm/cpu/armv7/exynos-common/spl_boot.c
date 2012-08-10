9deaddac0bff9a665336702f6cab708ac8edc1c7
Alim Akhtar <alim.akhtar@samsung.com>
2012-08-10 10:09:47 +0530

    exynos: spl: add a custom spi copy function
    This CL implements a custom spi_copy funtion to copy u-boot from SF to
    RAM. This is faster then iROM spi_copy funtion as this runs spi at 50Mhz
    and also in WORD mode of operation.
    
    BUG=chrome-os-partner:11288
    TEST=booted on snow can see significant improvement in spl boot time
         booted kernel till login screen
    
    Timer summary in microseconds:
           Mark    Elapsed  Stage
           0          0  reset
       100,000    100,000  spl_start
       227,818    127,818  board_init_f
    
    ....before this CL the boot time was as belwo:
    Timer summary in microseconds:
           Mark    Elapsed  Stage
           0          0  reset
       100,000    100,000  spl_start
       758,032    658,032  board_init_f
    
    Change-Id: Ice0ba23c52f797ee24142e4ad5c34e0c81fe91a9
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/29394

--- arch/arm/cpu/armv7/exynos-common/spl_boot.c
+++ arch/arm/cpu/armv7/exynos-common/spl_boot.c
@@ -22,7 +22,11 @@
 
 #include <common.h>
 #include <config.h>
+#include <spi.h>
 #include <asm/arch/board.h>
+#include <asm/arch/clock.h>
+#include <asm/arch-exynos/spi.h>
+#include <asm/arch/pinmux.h>
 #include <asm/arch/power.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -71,6 +75,103 @@ static int config_branch_prediction(int set_cr_z)
 	return cr & CR_Z;
 }
 
+static void spi_rx_tx(struct exynos_spi *regs, int todo,
+			void *dinp, void const *doutp, int i)
+{
+	uint *rxp = (uint *)(dinp + (i * (32 * 1024)));
+	int rx_lvl, tx_lvl;
+	uint out_bytes, in_bytes;
+
+	out_bytes = in_bytes = todo;
+	setbits_le32(&regs->ch_cfg, SPI_CH_RST);
+	clrbits_le32(&regs->ch_cfg, SPI_CH_RST);
+	writel(((todo * 8) / 32) | SPI_PACKET_CNT_EN, &regs->pkt_cnt);
+
+	while (in_bytes) {
+		int temp;
+
+		uint32_t spi_sts = readl(&regs->spi_sts);
+		rx_lvl = ((spi_sts >> 15) & 0x7f);
+		tx_lvl = ((spi_sts >> 6) & 0x7f);
+		if (tx_lvl < 60 && out_bytes) {
+			temp = 0xffffffff;
+			writel(temp, &regs->tx_data);
+			out_bytes -= 4;
+		}
+		if (rx_lvl >= 4 && in_bytes) {
+			temp = readl(&regs->rx_data);
+			if (rxp)
+				*rxp++ = be32_to_cpu(temp);
+			in_bytes -= 4;
+		}
+	}
+}
+
+/**
+ * Copy uboot from spi flash to RAM
+ *
+ * @parma uboot_size	size of u-boot to copy
+ */
+static void exynos_spi_copy(unsigned int uboot_size)
+{
+	int upto, todo;
+	int i;
+	struct exynos_spi *regs = (struct exynos_spi *)samsung_get_base_spi1();
+
+	clock_set_rate(PERIPH_ID_SPI1, 50000000); /* set spi clock to 50Mhz */
+	/* set the spi1 GPIO */
+	exynos_pinmux_config(PERIPH_ID_SPI1, PINMUX_FLAG_NONE);
+
+	/* set pktcnt and enable it */
+	writel(4 | SPI_PACKET_CNT_EN, &regs->pkt_cnt);
+	/* set FB_CLK_SEL */
+	writel(SPI_FB_DELAY_180, &regs->fb_clk);
+	/* set CH_WIDTH and BUS_WIDTH as word */
+	setbits_le32(&regs->mode_cfg, SPI_MODE_CH_WIDTH_WORD |
+					SPI_MODE_BUS_WIDTH_WORD);
+	clrbits_le32(&regs->ch_cfg, SPI_CH_CPOL_L); /* CPOL: active high */
+
+	/* clear rx and tx channel if set priveously */
+	clrbits_le32(&regs->ch_cfg, SPI_RX_CH_ON | SPI_TX_CH_ON);
+
+	/* do a soft reset */
+	setbits_le32(&regs->ch_cfg, SPI_CH_RST);
+	clrbits_le32(&regs->ch_cfg, SPI_CH_RST);
+
+	/* now set rx and tx channel ON */
+	setbits_le32(&regs->ch_cfg, SPI_RX_CH_ON | SPI_TX_CH_ON | SPI_CH_HS_EN);
+	clrbits_le32(&regs->cs_reg, SPI_SLAVE_SIG_INACT); /* CS low */
+
+	/* Send read instruction (0x3h) followed by a 24 bit addr */
+	writel((SF_READ_DATA_CMD << 24) | SPI_FLASH_UBOOT_POS, &regs->tx_data);
+
+	/* waiting for TX done */
+	while (!(readl(&regs->spi_sts) & SPI_ST_TX_DONE));
+
+	for (upto = 0, i = 0; upto < uboot_size; upto += todo, i++) {
+		todo = min(uboot_size - upto, (1 << 15));
+		spi_rx_tx(regs, todo, (void *)(CONFIG_SYS_TEXT_BASE),
+					(void *)(SPI_FLASH_UBOOT_POS), i);
+	}
+
+	setbits_le32(&regs->cs_reg, SPI_SLAVE_SIG_INACT);/* make the CS high */
+
+	/*
+	 * Let put controller mode to BYTE as
+	 * SPI driver does not support WORD mode yet
+	 */
+	clrbits_le32(&regs->mode_cfg, SPI_MODE_CH_WIDTH_WORD |
+					SPI_MODE_BUS_WIDTH_WORD);
+
+	/*
+	 * Flush spi tx, rx fifos and reset the SPI controller
+	 * and clear rx/tx channel
+	 */
+	clrsetbits_le32(&regs->ch_cfg, SPI_CH_HS_EN, SPI_CH_RST);
+	clrbits_le32(&regs->ch_cfg, SPI_CH_RST);
+	clrbits_le32(&regs->ch_cfg, SPI_TX_CH_ON | SPI_RX_CH_ON);
+}
+
 /* Copy U-Boot image to RAM */
 static void copy_uboot_to_ram(void)
 {
@@ -80,9 +181,6 @@ static void copy_uboot_to_ram(void)
 	enum boot_mode boot_mode;
 	mmc_copy_func_t mmc_copy;
 
-#if defined(CONFIG_EXYNOS_SPI_BOOT)
-	spi_copy_func_t spi_copy;
-#endif
 	usb_copy_func_t usb_copy;
 
 	uboot_size = exynos_get_uboot_size();
@@ -102,9 +200,8 @@ static void copy_uboot_to_ram(void)
 	switch (boot_mode) {
 #if defined(CONFIG_EXYNOS_SPI_BOOT)
 	case BOOT_MODE_SERIAL:
-		spi_copy = *(spi_copy_func_t *)EXYNOS_COPY_SPI_FNPTR_ADDR;
-		spi_copy(SPI_FLASH_UBOOT_POS, uboot_size,
-				CONFIG_SYS_TEXT_BASE);
+		/* let us our own function to copy u-boot from SF */
+		exynos_spi_copy(uboot_size);
 		break;
 #endif
 	case BOOT_MODE_MMC:
