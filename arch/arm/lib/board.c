d8c35161d4be3d3f4ca7c62bb5104e58f682b38b
Simon Glass <sjg@chromium.org>
2012-08-20 04:15:34 +0100

    Revert "arm: Add CONFIG_DISPLAY_BOARDINFO_LATE to display board info on LCD"
    This commit doesn't operate in our tree, and isn't used. Revert it so
    we can add the correct one.
    
    This reverts commit 4b8ed3d1738a6ff262bf0192e3eea8f8866398ba.
    
    BUG=none
    BRANCH=snow
    TEST=manual
    build and boot to kernel on snow
    
    Change-Id: Ic9272d49cb98c177caaf38b21ccda5803b036f09
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/30855
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -630,15 +630,6 @@ void board_init_r(gd_t *id, ulong dest_addr)
 
 	console_init_r();	/* fully init console as a device */
 
-#ifdef CONFIG_DISPLAY_BOARDINFO_LATE
-# ifdef CONFIG_OF_CONTROL
-	/* Put this here so it appears on the LCD, now it is ready */
-	printf("Model: %s\n", fdt_decode_get_model(gd->fdt_blob));
-# else
-	checkboard();
-# endif
-#endif
-
 #if defined(CONFIG_ARCH_MISC_INIT)
 	/* miscellaneous arch dependent initialisations */
 	arch_misc_init();
