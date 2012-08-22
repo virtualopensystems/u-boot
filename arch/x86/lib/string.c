cb156f94cab6e6051dd69eda8dafa72207e8e028
Stefan Reinauer <reinauer@chromium.org>
2012-08-22 16:16:54 -0700

    x86: Add optimized memcpy implementation
    This patch adds coreboot's speed optimized memcpy()
    to u-boot on x86.
    
    BUG=chrome-os-partner:12896
    TEST=boot Link with native resolution images, see that the pictures
         are displayed much faster.
    BRANCH=link
    Signed-off-by: Stefan Reinauer <reinauer@chromium.org>
    
    Change-Id: I5ce04a1330711bcff25726bfbfdcecb193be9a29
    Reviewed-on: https://gerrit.chromium.org/gerrit/31172
    Tested-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Ronald G. Minnich <rminnich@chromium.org>
    Commit-Ready: Stefan Reinauer <reinauer@chromium.org>

--- arch/x86/lib/string.c
+++ arch/x86/lib/string.c
@@ -85,3 +85,19 @@ void *memset(void *dstpp, int c, size_t len)
 
 	return dstpp;
 }
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned long d0, d1, d2;
+
+	asm volatile(
+		"rep ; movsl\n\t"
+		"movl %4,%%ecx\n\t"
+		"rep ; movsb\n\t"
+		: "=&c" (d0), "=&D" (d1), "=&S" (d2)
+		: "0" (n >> 2), "g" (n & 3), "1" (dest), "2" (src)
+		: "memory"
+	);
+
+	return dest;
+}
