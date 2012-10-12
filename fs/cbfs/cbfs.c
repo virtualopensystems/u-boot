293e13b969ea616aff516be69a18be94a8bd6344
Stefan Reinauer <reinauer@chromium.org>
2012-10-11 18:17:13 -0700

    gen: fix CBFS consistency checks
    The len field of the CBFS file header describes the len of the
    actual file stored, not including the CBFS header. This check
    broke adding files that are smaller than a CBFS header, so removing
    it.
    
    Signed-off-by: Stefan Reinauer <reinauer@chromium.org>
    
    BUG=chrome-os-partner:6108
    TEST=Add a file with 16 bytes to the legacy CBFS, see legacy mode
         work again.
    BRANCH=link
    
    Change-Id: I49949c51580f4a8571924d7a4a82996c351e4378
    Reviewed-on: https://gerrit.chromium.org/gerrit/35374
    Tested-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Ready: Stefan Reinauer <reinauer@chromium.org>

--- fs/cbfs/cbfs.c
+++ fs/cbfs/cbfs.c
@@ -110,8 +110,7 @@ static int file_cbfs_next_file(u8 *start, u32 size, u32 align,
 		}
 
 		swap_file_header(&header, fileHeader);
-		if (header.offset < sizeof(CbfsFileHeader) ||
-				header.offset > header.len) {
+		if (header.offset < sizeof(CbfsFileHeader)) {
 			file_cbfs_result = CBFS_BAD_FILE;
 			return -1;
 		}
