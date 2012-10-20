b4dc3b8a538f3327407921e16b31f3f7658ef0ff
Simon Glass <sjg@chromium.org>
2012-10-20 14:09:17 -0700

    cros: Obtain firmware type from fdt, and add a test
    U-Boot can act as RO, RW-A or RW-B firmware. A property exists in the fdt
    to indicate which firmware type is running. Add a function to read this
    property, and add a test for it also.
    
    BUG=chrome-os-partner:14545
    BRANCH=link
    TEST=build and boot on link, type 'vboot_twostop' see that it boots
    correctly.
    
    Change-Id: I862c21ef97f49a9ce4a011cb24cf4628b79f90e3
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/36207
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>

--- cros/include/cros_fdtdec.h
+++ cros/include/cros_fdtdec.h
@@ -24,6 +24,13 @@ struct fdt_chrome_ec {
 	int flash_erase_value;
 };
 
+/* Firmware type as given by the fdt */
+enum cros_firmware_type {
+	CROS_FIRMWARE_RO,
+	CROS_FIRMWARE_RW_A,
+	CROS_FIRMWARE_RW_B,
+};
+
 /* Decode Chrome OS specific configuration from fdt */
 
 int cros_fdtdec_flashmap(const void *fdt, struct twostop_fmap *config);
@@ -80,4 +87,16 @@ int cros_fdtdec_memory(const void *blob, const char *name,
  */
 int cros_fdtdec_chrome_ec(const void *blob, struct fdt_chrome_ec *config);
 
+/**
+ * Returns the current firmware type
+ *
+ * The fdt indicates whether we are in RO/RW-A/RW-B firmware. This function
+ * provides a way to read this information.
+ *
+ * @param blob		FDT blob to use
+ * @param typep		Returns firmware type
+ * @return 0 if ok, -1 on error (e.g. property not in fdt)
+ */
+int cros_fdtdec_firmware_type(const void *blob,
+			      enum cros_firmware_type *typep);
 #endif /* CROS_FDTDEC_H_ */
