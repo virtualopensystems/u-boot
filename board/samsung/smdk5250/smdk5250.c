127befabb40116b871d98746e94103d17411f2e4
Simon Glass <sjg@chromium.org>
2012-11-01 17:04:29 -0700

    exynos: Add error checking to functions called by ft_board_setup()
    These functions can fail, so return a proper error code.
    
    BUG=chromium-os:35959
    BRANCH=none
    TEST=build for snow
    
    Change-Id: If19356d1ff3a813558d5afc43aaca06e58620ec4
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/37171
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>

--- board/samsung/smdk5250/smdk5250.c
+++ board/samsung/smdk5250/smdk5250.c
@@ -370,8 +370,12 @@ static int board_i2c_arb_init(const void *blob)
 /**
  * Fix-up the kernel device tree so the bridge pd_n and rst_n gpios accurately
  * reflect the current board rev.
+ *
+ * @param blob		Device tree blob
+ * @param bd		Pointer to board information
+ * @return 0 if ok, -1 on error (e.g. not enough space in fdt)
  */
-static void ft_board_setup_gpios(void *blob, bd_t *bd)
+static int ft_board_setup_gpios(void *blob, bd_t *bd)
 {
 	int ret, rev, np, len;
 	const struct fdt_property *prop;
@@ -379,7 +383,7 @@ static void ft_board_setup_gpios(void *blob, bd_t *bd)
 	/* Do nothing for newer boards */
 	rev = board_get_revision();
 	if (rev < 4 || rev == 6)
-		return;
+		return 0;
 
 	/*
 	 * If this is an older board, replace powerdown-gpio contents with that
@@ -388,34 +392,40 @@ static void ft_board_setup_gpios(void *blob, bd_t *bd)
 	np = fdtdec_next_compatible(blob, 0, COMPAT_NXP_PTN3460);
 	if (np < 0) {
 		debug("%s: Could not find COMPAT_NXP_PTN3460\n", __func__);
-		return;
+		return -1;
 	}
 
 	prop = fdt_get_property(blob, np, "reset-gpio", &len);
 	if (!prop) {
 		debug("%s: Could not get property err=%d\n", __func__, len);
-		return;
+		return -1;
 	}
 
 	ret = fdt_setprop_inplace(blob, np, "powerdown-gpio", prop->data,
 			len);
 	if (ret) {
 		debug("%s: Could not setprop inplace err=%d\n", __func__, ret);
-		return;
+		return -1;
 	}
 
 	ret = fdt_delprop(blob, np, "reset-gpio");
 	if (ret) {
 		debug("%s: Could not delprop err=%d\n", __func__, ret);
-		return;
+		return -1;
 	}
+
+	return 0;
 }
 
 /**
  * Fix-up the kernel device tree so the powered-while-resumed is added to MP
  * device tree.
+ *
+ * @param blob		Device tree blob
+ * @param bd		Pointer to board information
+ * @return 0 if ok, -1 on error (e.g. not enough space in fdt)
  */
-static void ft_board_setup_tpm_resume(void *blob, bd_t *bd)
+static int ft_board_setup_tpm_resume(void *blob, bd_t *bd)
 {
 	const char kernel_tpm_compat[] = "infineon,slb9635tt";
 	const char prop_name[] = "powered-while-suspended";
@@ -424,28 +434,34 @@ static void ft_board_setup_tpm_resume(void *blob, bd_t *bd)
 	/* Only apply fixup to MP machine */
 	rev = board_get_revision();
 	if (!(rev == 0 || rev == 3))
-		return;
+		return 0;
 
 	node = fdt_node_offset_by_compatible(blob, 0, kernel_tpm_compat);
 	if (node < 0) {
 		debug("%s: fail to find %s: %d\n", __func__,
 				kernel_tpm_compat, node);
-		return;
+		return -1;
 	}
 
 	err = fdt_setprop(blob, node, prop_name, NULL, 0);
 	if (err) {
 		debug("%s: fail to setprop: %d\n", __func__, err);
-		return;
+		return -1;
 	}
-}
-
-int ft_board_setup(void *blob, bd_t *bd)
-{
-	ft_board_setup_gpios(blob, bd);
-	ft_board_setup_tpm_resume(blob, bd);
 
 	return 0;
+}
+
+int ft_system_setup(void *blob, bd_t *bd)
+{
+	if (ft_board_setup_gpios(blob, bd))
+		return -1;
+	return ft_board_setup_tpm_resume(blob, bd);
+}
+
+__weak int ft_board_setup(void *blob, bd_t *bd)
+{
+	return ft_system_setup(blob, bd);
 }
 
 #ifdef CONFIG_TPS65090_POWER
