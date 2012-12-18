6d6cfe75ccb2d6cb9756f50b9307ada4e12dfdc2
Simon Glass <sjg@chromium.org>
2012-12-18 12:49:45 -0800

    x86: config: Don't use CONFIG_OF_CBFS in coreboot
    Now that we are moving to using a flat binary, we will just append the
    device tree as with other architectures.
    
    BUG=chrome-os-partner:13961
    BRANCH=link
    TEST=manual
    build and boot to kernel on link
    
    Change-Id: I14a3ae31b519f9eb696918fe9adb9469b6ba3c9c
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/39944
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>

--- include/configs/coreboot.h
+++ include/configs/coreboot.h
@@ -55,7 +55,6 @@
 #define CONFIG_OF_LIBFDT	/* Device tree support */
 #define CONFIG_OF_CONTROL	/* Use the device tree to set up U-Boot */
 #define CONFIG_DEFAULT_DEVICE_TREE      link
-#define CONFIG_OF_CBFS
 #define CONFIG_OF_SEPARATE
 
 /*-----------------------------------------------------------------------
