virtualopensystems/u-boot

commit 34c2b59a881006c5fa2474be54ab0e440d8e32a9

exynos: Fix up kernel device tree for TPM resume
On MP device, TPM is powered while system is suspended; so kernel has to
know whether it is running on a MP device, and go different code paths
on resume.

Let U-Boot probe board revision and fix up kernel device tree before
booting kernel.  If it is a MP device, powered-while-suspended is added
to kernel device tree's tpm node.

Signed-off-by: Che-Liang Chiou <clchiou@chromium.org>

BRANCH=snow
BUG=chrome-os-partner:13192
TEST=boot Snow MP and check
       [ -e /proc/device-tree/i2c\@12C90000/tpm/powered-while-suspended ]
     boot Snow DVT/PVT and check
       [ ! -e /proc/device-tree/i2c\@12C90000/tpm/powered-while-suspended ]

Change-Id: I09eb528b7140a324368743a841fe7b89f84220c4
Reviewed-on: https://gerrit.chromium.org/gerrit/31780
Commit-Ready: Che-Liang Chiou <clchiou@chromium.org>
Tested-by: Che-Liang Chiou <clchiou@chromium.org>
Reviewed-by: Vic Yang <victoryang@chromium.org>
Reviewed-by: Simon Glass <sjg@chromium.org>

board/samsung/smdk5250/smdk5250.c
@@ -348,7 +348,7 @@ static int board_i2c_arb_init(const void *blob)
  * Fix-up the kernel device tree so the bridge pd_n and rst_n gpios accurately
  * reflect the current board rev.
  */
-void ft_board_setup(void *blob, bd_t *bd)
+static void ft_board_setup_gpios(void *blob, bd_t *bd)
 {
 	int ret, rev, np, len;
 	const struct fdt_property *prop;
@@ -386,6 +386,41 @@ void ft_board_setup(void *blob, bd_t *bd)
 		debug("%s: Could not delprop err=%d\n", __func__, ret);
 		return;
 	}
+}
+
+/**
+ * Fix-up the kernel device tree so the powered-while-resumed is added to MP
+ * device tree.
+ */
+static void ft_board_setup_tpm_resume(void *blob, bd_t *bd)
+{
+	const char kernel_tpm_compat[] = "infineon,slb9635tt";
+	const char prop_name[] = "powered-while-suspended";
+	int err, node, rev;
+
+	/* Only apply fixup to MP machine */
+	rev = board_get_revision();
+	if (!(rev == 0 || rev == 3))
+		return;
+
+	node = fdt_node_offset_by_compatible(blob, 0, kernel_tpm_compat);
+	if (node < 0) {
+		debug("%s: fail to find %s: %d\n", __func__,
+				kernel_tpm_compat, node);
+		return;
+	}
+
+	err = fdt_setprop(blob, node, prop_name, NULL, 0);
+	if (err) {
+		debug("%s: fail to setprop: %d\n", __func__, err);
+		return;
+	}
+}
+
+void ft_board_setup(void *blob, bd_t *bd)
+{
+	ft_board_setup_gpios(blob, bd);
+	ft_board_setup_tpm_resume(blob, bd);
 }
 
 #ifdef CONFIG_TPS65090_POWER

arch/arm/lib/bootm.c
@@ -208,6 +208,9 @@ static int bootm_linux_fdt(int machid, bootm_headers_t *images)
 		return ret;
 
 #ifdef CONFIG_OF_BOARD_SETUP
+	/* Try to reserve 1024 bytes for board fixups */
+	if (!fdt_open_into(*of_flat_tree, *of_flat_tree, of_size + 1024))
+		of_size += 1024;
 	/* Call the board-specific fixup routine */
 	ft_board_setup(*of_flat_tree, gd->bd);
 #endif

commit 83e8809dfb56eecb36d9faff351dcacd72455cd0

exynos: Don't abort boot when no TPM found
Commit commit 34c2b59a8 (exynos: Fix up kernel device tree for TPM resume)
introduced modification of the TPM device node, unfortunately it fails
boot completely if the node is not found.

Instead of failing, continue booting.

BUG=none
TEST=boot a mainline kernel lacking tpm node

Change-Id: I2d789ba95c231ba347a17d025627b37fdbb35c78
Signed-off-by: Olof Johansson <olofj@chromium.org>
Reviewed-on: https://gerrit.chromium.org/gerrit/49265
Reviewed-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>

board/samsung/smdk5250/smdk5250.c
@@ -496,14 +496,14 @@ static int ft_board_setup_tpm_resume(void *blob, bd_t *bd)
 
 	node = fdt_node_offset_by_compatible(blob, 0, kernel_tpm_compat);
 	if (node < 0) {
-		debug("%s: fail to find %s: %d\n", __func__,
+		debug("%s: Could not find %s: %d\n", __func__,
 				kernel_tpm_compat, node);
-		return -1;
+		return 0;
 	}
 
 	err = fdt_setprop(blob, node, prop_name, NULL, 0);
 	if (err) {
-		debug("%s: fail to setprop: %d\n", __func__, err);
+		debug("%s: Could not setprop: %d\n", __func__, err);
 		return -1;
 	}
 