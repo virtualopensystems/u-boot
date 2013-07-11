343633072e503060591bf4bc977a0e81f357c135
Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
2013-07-11 15:55:03 +0300

    Embed dtc
    ChromiumOS' U-Boot requires a specific dtc version.
    This patch adds a functionality to build.sh which will git checkout
    the proper version, compile it and delete the source. Than this dtc
    will be used.
    All this is done under scripts folder.
    
    Signed-off-by: Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,13 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
+
+TOP=$(pwd)
+SCRIPTS=${TOP}/scripts
 
 export CROSS_COMPILE=arm-linux-gnueabihf-
+export DTC=${SCRIPTS}/dtc
+export PATH=${SCRIPTS}:$PATH
 
 TEXT_START=$(grep CONFIG_SYS_TEXT_BASE include/configs/exynos5-common.h | awk '{ print $3 }')
 NV_NAME=nv_uboot-snow
@@ -9,6 +16,19 @@ KPART=${NV_NAME}.kpart
 
 # cleanup previous build binaries 
 rm -f ${NV_NAME}*
+
+# get the proper dtc version
+if [ ! -x ${DTC} ]
+then 
+	DTCSRC=${SCRIPTS}/dtc-src
+	rm -rf ${DTCSRC}
+	git clone http://git.chromium.org/chromiumos/third_party/dtc.git ${DTCSRC}
+	cd ${DTCSRC}
+	make -j4
+	cp dtc ${DTC}
+	cd ${TOP}
+	rm -rf ${DTCSRC}
+fi
 
 # clean, configure and build
 make distclean
